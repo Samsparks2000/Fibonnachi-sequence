8e89f1235b9b9f0402a969bf1f8b67fd20142624
Samsparks <ssamsparks@gmail.com>
2023-09-23 13:51:17 +0100

    My first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#fibbonachi sequence
+import math
+seq = int(input("select how long you want the fibonacci seq to be: "))
+
+def fib(n):
+    x = [0, 1]
+    for i in range(n):
+        y = x[len(x) - 1] + x[len(x) - 2]
+        x.append(y)
+    return x
+
+
+print(f"{fib(seq)})")
+
+
+
+
